5fa8c61f95ea09247e9cc817ceae93f57aa79c2c
rafaseron <rafaelseron3011@gmail.com>
2023-09-05 17:03:19 -0400

    if and when test

--- run.kt
+++ run.kt
@@ -1,4 +1,23 @@
-fun main(){
+fun main() {
+    //readln test
     val testeNome = readln()
     println("seu nome é $testeNome")
+
+    //if test
+    if (testeNome == "rafael") {
+        println("meu nome tambem eh rafael :D (usando if)")
+    } else{
+        println("é você não se chama rafael mesmo ... (usando if)")
+    }
+
+    //when test
+    when (testeNome){
+        "rafael" -> println("meu nome também eh rafael :D (usando when)")
+        "marcos" -> println("prazer marcos, meu nome é rafael (usando when)")
+         else -> println("poxa você não se chama rafael nem marcos, mas tabom né (usando when)")
+
+    }
+
+
+
 }
