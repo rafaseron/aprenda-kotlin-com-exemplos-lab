ae4c73ce6f9c0b94e46449de6475ef5ae3c58d9d
rafaseron <rafaelseron3011@gmail.com>
2023-09-05 18:00:53 -0400

    conditionals n loops

--- run.kt
+++ run.kt
@@ -1,7 +1,9 @@
 fun main() {
     //readln test
-    val testeNome = readln()
+    var testeNome = readln()
     println("seu nome é $testeNome")
+
+  //CONDIÇÕES
 
     //if test
     if (testeNome == "rafael") {
@@ -14,10 +16,25 @@ fun main() {
     when (testeNome){
         "rafael" -> println("meu nome também eh rafael :D (usando when)")
         "marcos" -> println("prazer marcos, meu nome é rafael (usando when)")
-         else -> println("poxa você não se chama rafael nem marcos, mas tabom né (usando when)")
-
+         else -> {
+             println("poxa você não se chama rafael nem marcos, mas tabom né (usando when).")
+        }
     }
 
+  //LOOPS
+    while (testeNome != "marcos"){
+        println("apenas um marcos pode continuar executando este programa (usando while)")
+        println("esta é a ultima chance para você se chamar marcos: ")
+        testeNome = readln()
+        continue
+    }
 
+    do {
+        println("oi marcos (usando do while)")
+        break //break test
+    } while(testeNome == "marcos")
+
+  //WORKING WITH ARRAYS
+  // FOR LOOPS IS THE BEST WAY TO JUMP THROUGH OF THE ELEMENTS
 
 }
