e3b06c34432d310b9da0816ef11f58a251d71136
rafaseron <rafaelseron3011@gmail.com>
2023-09-12 22:12:28 -0400

    parece que deu certo

--- desafio.kt
+++ desafio.kt
@@ -1,5 +1,5 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
+/*
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
@@ -19,3 +19,4 @@ fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
+*/
