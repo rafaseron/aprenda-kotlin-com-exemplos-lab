6ccfb6a84318e05c897d476e9435e300fb61afad
rafaseron <rafaelseron3011@gmail.com>
2023-09-13 04:22:18 -0400

    terminei lists

--- src/main/kotlin/run.kt
+++ src/main/kotlin/run.kt
@@ -9,12 +9,12 @@ fun main() {
     //if test
     if (testeNome == "rafael") {
         println("meu nome tambem eh rafael :D (usando if)")
-    } else{
+    } else {
         println("é você não se chama rafael mesmo ... (usando if)")
     }
 
     //when test
-    when (testeNome){
+    when (testeNome) {
         "rafael" -> println("meu nome também eh rafael :D (usando when)")
         "marcos" -> println("prazer marcos, meu nome é rafael (usando when)")
         else -> {
@@ -23,7 +23,7 @@ fun main() {
     }
 
     //LOOPS
-    while (testeNome != "marcos"){
+    while (testeNome != "marcos") {
         println("apenas um marcos pode continuar executando este programa (usando while)")
         println("esta é a ultima chance para você se chamar marcos: ")
         testeNome = readln()
@@ -33,7 +33,7 @@ fun main() {
     do {
         println("oi marcos (usando do while)")
         break //break test
-    } while(testeNome == "marcos")
+    } while (testeNome == "marcos")
 
     //WORKING WITH ARRAYS
     val nomes = arrayOf("rafael", "marcos", "adalberto")
@@ -50,27 +50,27 @@ fun main() {
     println(nomes.size)
 
     // check if and element exists
-    if ("rafael" in nomes){
+    if ("rafael" in nomes) {
         println("ele existe no banco de dados")
-    } else{
+    } else {
         println("rafael não existe mais no nosso banco de dados")
     }
 
     // FOR LOOPS IS THE BEST WAY TO JUMP THROUGH OF THE ELEMENTS
     // agora que já vimos como checkar se um elemento existe em um array, vamos então percorre-lo por completo
     println("PERCORRENDO O ARRAY INTEIRO UTILIZANDO FOR")
-    for (i in nomes){
+    for (i in nomes) {
         println(i)
     }
 
     //RANGES só pra brincar ne
-    for (i in 'a'..'z'){
+    for (i in 'a'..'z') {
         print("amazon é de A a ")
         println(i)
     }
 
     //KOTLIN FUNCTIONS
-    fun frasePronta(){
+    fun frasePronta() {
         val nome1 = nomes[0]
         val nome2 = nomes[1]
         print("o primeiro nome é $nome1 e o segundo é o $nome2")
@@ -78,23 +78,62 @@ fun main() {
     frasePronta()
 
     //agora passando argumentos
-    fun outraFrase(nome3: String, nome4: String){
+    fun outraFrase(nome3: String, nome4: String) {
         println("Os dois nomes são: $nome3 + $nome4")
     }
     outraFrase("joao", "maria")
 
     //agora vamos retonar um resultado
-    fun soma (valor1: Int, valor2: Int): Int{
-        return(valor1+valor2)
+    fun soma(valor1: Int, valor2: Int): Int {
+        return (valor1 + valor2)
     }
     println("agora vamos testar a função que retorna resultados")
-    val resultado = soma(5,2)
+    val resultado = soma(5, 2)
     println(resultado)
     //instanciando a função direto no println
     println("agora vamos instanciar a função direto no println")
-    println(soma(5,2))
+    println(soma(5, 2))
 
     // COLEÇÕES EM KOTLIN
+    /* o objetivo é escolher a melhor coleção que vai ARMAZENAR seus dados. isso
+    vai de acordo com o dado que você quer guardar e o tipo de ORGANIZAÇÃO
+    a qual você precisa */
+
+    // LISTAS (literalmente uma lista)
+    val listNomes = listOf("rafael", "marcos", "adalberto") //é igual um Array
+    println("nesta lista temos: $listNomes")
+
+    println("o primeiro elemento desta lista é impresso igual um array: ${listNomes[0]}")
+    /* para imprimir determinado elemento de um ARRAY ou LISTA, quando feito diretamente
+    no println, precisamos utilizar { } com a variavel dentro
+    (isso por causa dos colchetes [] )*/
+
+    //PODEMOS UTILIZAR AQUI TAMBÉM A FUNÇÃO SIZE (.size)
+    val tamanhoLista = listNomes.size   //isso é como eu faria antigamente
+    // aqui estamos instanciando em um objeto e imprimiremos o objeto
+    println("o tamanho da lista é: $tamanhoLista")
+    println(listNomes.size) //igual o modo antigo, mas sem adicionar texto
+
+    //AQUI DIRETO SEM INSTANCIAR ATOA (serve para array também) - com chaves
+    println("o tamanho da lista é: ${listNomes.size} ")
+
+    //temos como procurar qual o indice de um elemento com .indexOf
+    println("o indice do elemento marcos é: ${listNomes.indexOf("marcos")}")
+
+    //temos como verificar se um elemento existe nesta lista
+    if ("marcos" in listNomes){
+        println("existe marcos dentro desta lista")
+    }
+
+    // SETS (como um saco de bolinhas de gude diferentes)
+    /* é um conjunto de mesmo tipo de dados, não pode se repetir
+    e não importa a ordem */
+
+
+
+
+
+    // MAPS (encontra um VALOR utilizando sua CHAVE correspondente)
 
 
     // KOTLIN CLASSES
