229283eb0c81c15337f4cdc58d2877231662a15a
rafaseron <rafaelseron3011@gmail.com>
2023-09-14 04:47:54 -0400

    como diferentes tipos de classe se relacionam?

--- src/main/kotlin/desafio.kt
+++ src/main/kotlin/desafio.kt
@@ -10,6 +10,7 @@ data class Curso (var nome: String, val duracao: Int = 1)
 data class Formacao(val nome: String, var conteudos: List<Curso>) {
 
     val inscritos = mutableListOf<Usuario>()
+    //lista mutavel criada especificamente para armazenar objetos da classe Usuario
     
     fun matricular(usuario: Usuario) {
         inscritos.add(usuario)
@@ -17,6 +18,7 @@ data class Formacao(val nome: String, var conteudos: List<Curso>) {
     }
     fun desmatricular (usuario:Usuario){
         inscritos.remove(usuario)
+        println("$usuario se desmatriculou com sucesso")
     }
 }
 //voce vai ter que testar como adicionar um novo USUARIO a LISTA de inscritos
@@ -29,6 +31,20 @@ de todos os Cursos existentes na Formação, para dar a duração total da Forma
 no Curso e na Formacao */
 
 fun main() {
-    //DONE: TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    /* você precisará criar uma lista de Curso para adicionar em Formacao */
+
+    //criando um objeto
+    val user1 = Usuario("joao", 27, "masculino")
+    /* agora ao instanciar um objeto da classe Usuario, ta te faltando base
+    para entender a relação entre a Data class e a classe Usuario (uma classe normal) */
+
+    /* também to vendo que te falta base de enum class para entender como ela pode
+    se relacionar com uma Data class Curso e/ou com a Data class Formacao */
+
+    // CONSELHO: REVER estes conteudos e retornar posteriormente !!
+
+
+
+
 }
